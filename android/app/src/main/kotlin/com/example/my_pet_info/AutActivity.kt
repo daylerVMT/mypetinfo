6e33b571a91e97f043d4121bb22d7415d1e318d3
dayler <dayler.vmt7@gmail.com>
2023-11-06 01:23:51 -0400

    first update project

---
+++ android/app/src/main/kotlin/com/example/my_pet_info/AutActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_pet_info
+
+import io.flutter.embedding.android.FlutterActivity
+
+class AutActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_pet_info/features/app/splash_screen/splash_screen.dart';
+import 'package:my_pet_info/features/user_auth/presentation/pages/home_page.dart';
+import 'package:my_pet_info/features/user_auth/presentation/pages/login_page.dart';
+import 'package:my_pet_info/features/user_auth/presentation/pages/sign_up_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCNoWGo1SXOD0Kfd4SOZ8PgHwYqILbQbUI",
+        appId: "1:830369899680:web:e5a466f3e6f7bd70e895c5",
+        messagingSenderId: "830369899680",
+        projectId: "mypetinfo-day",
+        storageBucket: "mypetinfo-day.appspot.com",
+        measurementId: "G-VS5Q42HME9"
+        // web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
+      routes: {
+        '/': (context) => const SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
+        ),
+        '/login': (context) => const LoginPage(),
+        '/signUp': (context) => const SignUpPage(),
+        '/home': (context) => const HomePage(),
+      },
+    );
+  }
+}
